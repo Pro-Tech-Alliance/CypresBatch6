79397401ec92d708ed268cd554e25262d492f6b3
Prof Milo <emileesapa@gmail.com>
2024-03-08 11:51:35 +0100

    added more steps in user input

--- cypress/e2e/test-cases/web-inputs.cy.js
+++ cypress/e2e/test-cases/web-inputs.cy.js
@@ -53,6 +53,33 @@ it('Positive Testing User Input',()=>{
 
 
 it('Clear Users input',()=>{
+    cy.get('#input-number').type("12345")
+    cy.get('#input-text').type('Pro-Tech Alliance Batch#6')
+    cy.get('#input-password').type('HelloPeople12$5')
+    cy.get('#btn-clear-inputs').click()
+
+    //checking all inouts are cleared
+    cy.get('#input-number').should('be.empty')
+    cy.get('#input-text').should('be.empty')
+    cy.get('#input-password').should('be.empty')
+
+})
+
+it('Clearing users output with input',()=>{
+    cy.get('#input-number').type("12345")
+    cy.get('#input-text').type('Pro-Tech Alliance Batch#6')
+    cy.get('#input-password').type('HelloPeople12$5')
+
+    cy.get('#btn-display-inputs').click()//display button
+    cy.get('#output-text').should('be.visible')
+
+    cy.get('#btn-clear-inputs').click()//clear button
+    cy.wait(2000)
+    cy.get('#output-text').should('not.be.visible')
+    cy.get('#input-number').should('be.empty')
+    cy.get('#input-text').should('be.empty')
+    cy.get('#input-password').should('be.empty')
+
 
 
 

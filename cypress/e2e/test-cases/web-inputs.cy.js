dfd6d1f17b1b82e977d649733d66ee7be19b9b93
Akombo Neville Akwo <akomboneville55@gmail.com>
2024-03-12 11:58:46 +0100

    create alias for output-text and also changed assertion from not.be.visible to not.exist

--- cypress/e2e/test-cases/web-inputs.cy.js
+++ cypress/e2e/test-cases/web-inputs.cy.js
@@ -1,96 +1,79 @@
 /// <reference types="cypress" />
-import WebInputPage from "../pageObjects/webInputs-objects"
-import GeneralPage from "../pageObjects/generalObjects"
+import WebInputPage from "../pageObjects/webInputs-objects";
+import GeneralPage from "../pageObjects/generalObjects";
 
 const webinputs = new WebInputPage();
-const generalPage = new GeneralPage()
+const generalPage = new GeneralPage();
 
-describe('Validating Web Inputs', () => {
-    beforeEach(() => {
-        cy.visit('https://practice.expandtesting.com/inputs')
+describe("Validating Web Inputs", () => {
+  beforeEach(() => {
+    cy.visit("https://practice.expandtesting.com/inputs");
+  });
 
-    })
+  it("Negative Testing User input", () => {
+    generalPage.verifyPageTitle_is("Web inputs");
+    generalPage.verifyPageHeading_is("Web inputs");
 
-    it('Negative Testing User input', () => {
+    //Users input
+    webinputs.enterInputNumber().type("1234567890abcdefghiujgfhrt");
+    webinputs.enterInputText().type("KTYDSTCTYCT56724567*^%$^");
+    webinputs.enterInputPassword().type("123#ABC.y");
 
-        generalPage.verifyPageTitle_is('Web inputs')
-        generalPage.verifyPageHeading_is('Web inputs')
+    webinputs.displayInputButton().click();
 
+    //Display output
+    webinputs.displayOutputNumber().should("contain.text", "1234567890");
+    webinputs
+      .displayOutputText()
+      .should("contain.text", "KTYDSTCTYCT56724567*^%$^");
+    webinputs.displayOutputPassword().should("contain.text", "123#ABC.y");
+  });
 
-        //Users input
-        webinputs.enterInputNumber().type("1234567890abcdefghiujgfhrt")
-        webinputs.enterInputText().type("KTYDSTCTYCT56724567*^%$^")
-        webinputs.enterInputPassword().type('123#ABC.y')
+  it("Positive Testing User Input", () => {
+    generalPage.verifyPageTitle_is("Web inputs");
+    generalPage.verifyPageHeading_is("Web inputs");
 
-        webinputs.displayInputButton().click()
-      
-        //Display output
-        webinputs.displayOutputNumber().should('contain.text', '1234567890')
-        webinputs.displayOutputText().should('contain.text', 'KTYDSTCTYCT56724567*^%$^')
-        webinputs.displayOutputPassword().should('contain.text', '123#ABC.y')
+    //Users input
+    cy.get("#input-number").type("12345");
+    cy.get("#input-text").type("Pro-Tech Alliance Batch#6");
+    //123#ABC.y
+    cy.get("#input-password").type("HelloPeople12$5");
 
+    cy.get("#btn-display-inputs").click();
 
-    })
+    //Display output
+    cy.get("#output-number").should("contain.text", "12345");
+    cy.get("#output-text").should("contain.text", "Pro-Tech Alliance Batch#6");
+    cy.get("#output-password").should("contain.text", "HelloPeople12$5");
+  });
 
+  it("Clear Users input", () => {
+    cy.get("#input-number").type("12345");
+    cy.get("#input-text").type("Pro-Tech Alliance Batch#6");
+    cy.get("#input-password").type("HelloPeople12$5");
+    cy.get("#btn-clear-inputs").click();
 
+    //checking all inouts are cleared
+    cy.get("#input-number").should("be.empty");
+    cy.get("#input-text").should("be.empty");
+    cy.get("#input-password").should("be.empty");
+  });
 
+  it("Clearing users output with input", () => {
+    cy.get("#input-number").type("12345");
+    cy.get("#input-text").type("Pro-Tech Alliance Batch#6");
+    cy.get("#input-password").type("HelloPeople12$5");
 
-    it('Positive Testing User Input', () => {
+    cy.get("#btn-display-inputs").click(); //display button
+    //create an alias for output text
+    cy.get("#output-text").as("outputText");
+    cy.get("@outputText").should("be.visible");
 
-    
-        generalPage.verifyPageTitle_is('Web inputs')
-        generalPage.verifyPageHeading_is('Web inputs')
-
-        //Users input
-        cy.get('#input-number').type("12345")
-        cy.get('#input-text').type('Pro-Tech Alliance Batch#6')
-        //123#ABC.y
-        cy.get('#input-password').type('HelloPeople12$5')
-
-        cy.get('#btn-display-inputs').click()
-
-        //Display output
-        cy.get('#output-number').should('contain.text', '12345')
-        cy.get('#output-text').should('contain.text', 'Pro-Tech Alliance Batch#6')
-        cy.get('#output-password').should('contain.text', 'HelloPeople12$5')
-
-    })
-
-
-    it('Clear Users input', () => {
-        cy.get('#input-number').type("12345")
-        cy.get('#input-text').type('Pro-Tech Alliance Batch#6')
-        cy.get('#input-password').type('HelloPeople12$5')
-        cy.get('#btn-clear-inputs').click()
-
-        //checking all inouts are cleared
-        cy.get('#input-number').should('be.empty')
-        cy.get('#input-text').should('be.empty')
-        cy.get('#input-password').should('be.empty')
-
-    })
-
-    it('Clearing users output with input', () => {
-        cy.get('#input-number').type("12345")
-        cy.get('#input-text').type('Pro-Tech Alliance Batch#6')
-        cy.get('#input-password').type('HelloPeople12$5')
-
-        cy.get('#btn-display-inputs').click()//display button
-        cy.get('#output-text').should('be.visible')
-
-        cy.get('#btn-clear-inputs').click()//clear button
-        cy.wait(2000)
-        cy.get('#output-text').should('not.be.visible')
-        cy.get('#input-number').should('be.empty')
-        cy.get('#input-text').should('be.empty')
-        cy.get('#input-password').should('be.empty')
-
-
-
-
-    })
-
-
-
-
-})
+    cy.get("#btn-clear-inputs").click(); //clear button
+    cy.wait(2000);
+    cy.get("@outputText").should("not.exist");
+    cy.get("#input-number").should("be.empty");
+    cy.get("#input-text").should("be.empty");
+    cy.get("#input-password").should("be.empty");
+  });
+});

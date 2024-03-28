6471d1122f24f9fa4fb704d58a57693b67897e6f
Prof Milo <emileesapa@gmail.com>
2024-03-28 07:20:48 -0500

    fixtures done

--- cypress/e2e/test-cases/form-validation.cy.js
+++ cypress/e2e/test-cases/form-validation.cy.js
@@ -2,39 +2,42 @@
 
 import FormValidationPage from "../pageObjects/form-validationObjects";
 import GeneralPage from "../pageObjects/generalObjects";
+import myData from "../../fixtures/testData.json"
+const testData = require('../../fixtures/testData.json')
 const formValidation = new FormValidationPage()
 const generalPage = new GeneralPage()
 
 describe('Validating Web Inputs', () => {
     beforeEach(() => {
+    
         cy.visit('https://practice.expandtesting.com/form-validation')
 
     })
 
 
 
-it('Positf Test Form Validation', ()=>{
+    it('Positf Test Form Validation', () => {
 
-    generalPage.verifyPageHeading_is('Form Validation')
-    formValidation.InputContactName()
-    .clear()
-    .type('Pro-Tech Alliance')
+        generalPage.verifyPageHeading_is('Form Validation')
+        formValidation.InputContactName()
+            .clear()
+            .type(myData.testName)
 
-    formValidation.InputContactNumber().type('469-6942728')
-    formValidation.InputPickUpDate().type('01012024')
-    formValidation.SelectPaymentMethod().select('card')
-    formValidation.RegisterButton().click()
+        formValidation.InputContactNumber().type(myData.testContactNumber)
+        formValidation.InputPickUpDate().type(myData.testDate)
+        formValidation.SelectPaymentMethod().select(myData.selectPaymentMethod)
+        formValidation.RegisterButton().click()
 
-    generalPage.verifyPageHeading_is('Form Confirmation')
-    cy.get('alert').should('contain.text','Thank you for validating your ticket')
+        generalPage.verifyPageHeading_is('Form Confirmation')
+        cy.get('alert').should('contain.text', 'Thank you for validating your ticket')
 
-})
-
-
-it('Negative Test From Valiudation',()=>{
+    })
 
 
-})
+    it('Negative Test From Valiudation', () => {
+
+
+    })
 
 
 
